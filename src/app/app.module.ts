27f8e369c92d60b3f300f5abbebab21be91d9635
Allyson de Oliveira Brandão <allysomoliveira7@gmail.com>
2023-06-26 23:05:20 -0300

    Imports dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthGuard } from './authGuard/auth.guard';
+import { UserService } from './service/user/user.service';
+import { LoginService } from './service/login/login.service';
+import { AlertsService } from './service/Alerts/alerts.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    AuthGuard,
+    UserService,
+    LoginService,
+    AlertsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
