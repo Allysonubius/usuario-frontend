514198654681cd926f96b80b81c75d1bc6cbb8b7
Allyson de Oliveira Brandão <allysomoliveira7@gmail.com>
2023-06-27 10:18:55 -0300

    Definido rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { AuthGuard } from './authGuard/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login-user', component: LoginComponent },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+
+  // otherwise redirect to home
+  { path: '', redirectTo: 'home', pathMatch: 'full' }, // redireciona a rota vazia para /home
+  { path: '**', redirectTo: 'home' } // redireciona todas as outras rotas para /home
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
