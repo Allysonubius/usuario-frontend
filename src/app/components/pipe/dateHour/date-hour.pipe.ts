cd6fcdda31ade6c5c3c56fb4ab083653043694d8
Allyson de Oliveira Brandão <allysomoliveira7@gmail.com>
2023-08-03 21:42:22 -0300

    Criado listagem

--- src/app/components/pipe/dateHour/date-hour.pipe.ts
+++ src/app/components/pipe/dateHour/date-hour.pipe.ts
@@ -1,5 +1,4 @@
 import { Pipe, PipeTransform } from '@angular/core';
-
 @Pipe({
   name: 'dateHour'
 })
@@ -9,17 +8,17 @@ export class DateHourPipe implements PipeTransform {
     let formattedDateTime = '';
 
     if (value && value.length >= 14) {
-      const day = value.substring(0, 2);
-      const month = value.substring(2, 4);
-      const year = value.substring(4, 8);
+      const year = value.substring(0, 4);
+      const month = value.substring(4, 6);
+      const day = value.substring(6, 8);
+      
       const hour = value.substring(8, 10);
       const minute = value.substring(10, 12);
       const second = value.substring(12, 14);
 
-      formattedDateTime = `${day}/${month}/${year} ${hour}:${minute}:${second}`;
+      formattedDateTime = `${year}/${month}/${day} ${hour}:${minute}:${second}`;
     }
 
     return formattedDateTime;
   }
-
 }
